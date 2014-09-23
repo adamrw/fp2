d9c6430b8947260fdaff607e3918f2036f8da2b6
Adam Weiner <adamwineguy@gmail.com>
2014-09-23 14:57:13 -0600

    finished through #3 in function projects 2

--- main.js
+++ main.js
@@ -10,10 +10,20 @@ $(document).on('ready', function() {
 
 //   totalLetters
 
-var totalLetters=function(amount){
-	// amount.join(',');
-	return amount.length;
-};
-console.log(totalLetters(['javascript', 'is', 'awesome']), amount.join(',')));
+// var totalLetters=function(amount){
+// 	console.log(amount.join());
+// 	return amount.join('').length;
+// };
+// totalLetters(['what', 'happened', 'to', 'my', 'function']);
+// console.log(totalLetters(['javascript', 'is', 'awesome']));
 
+
+
+//    keyValue
+
+var keyValue=function (obj) {
+	return obj.name; 
+};
+
+console.log(keyValue('city', 'Denver'));
 });
