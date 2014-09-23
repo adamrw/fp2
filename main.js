6b02a7895b9d0286df5117253575b7d957000d9a
Adam Weiner <adamwineguy@gmail.com>
2014-09-23 14:21:29 -0600

    more functions practice

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).on('ready', function() {
+
+//   getName
+
+// var getName=function(obj){
+// 	return obj.name;
+// };
+// console.log(getName({ name: 'Luisa', age: 25 }));
+
+
+//   totalLetters
+
+var totalLetters=function(amount){
+	// amount.join(',');
+	return amount.length;
+};
+console.log(totalLetters(['javascript', 'is', 'awesome']), amount.join(',')));
+
+});
