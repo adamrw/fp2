96b83b23e7f61bbb4f2e401c93acf28a731d7138
Adam Weiner <adamwineguy@gmail.com>
2014-09-25 09:06:51 -0600

    updates for function project 2

--- main.js
+++ main.js
@@ -2,32 +2,32 @@ $(document).on('ready', function() {
 
 //   getName
 
-// var getName=function(obj){
-// 	return obj.name;
-// };
-// console.log(getName({ name: 'Luisa', age: 25 }));
+var getName=function(obj){
+	return obj.name;
+};
+console.log(getName({ name: 'Luisa', age: 25 }));
 
 
 //   totalLetters
 
-// var totalLetters=function(amount){
-// 	console.log(amount.join());
-// 	return amount.join('').length;
-// };
-// totalLetters(['what', 'happened', 'to', 'my', 'function']);
-// console.log(totalLetters(['javascript', 'is', 'awesome']));
+var totalLetters=function(amount){
+	console.log(amount.join());
+	return amount.join('').length;
+};
+totalLetters(['what', 'happened', 'to', 'my', 'function']);
+console.log(totalLetters(['javascript', 'is', 'awesome']));
 
 
 
 //    keyValue
 
-// var keyValue=function (city, name) {
-// 	var object={};
-// 	object[city]=name;
-// 	return object;
-// };
+var keyValue=function (city, name) {
+	var object={};
+	object[city]=name;
+	return object;
+};
 
-// console.log(keyValue('city', 'Denver'));
+console.log(keyValue('city', 'Denver'));
 
 
 
